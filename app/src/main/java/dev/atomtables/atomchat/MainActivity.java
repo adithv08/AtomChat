15207503b2a009b65cc1eb9c7f33da9fc165103b
adithv08 <adithv08@gmail.com>
2022-11-29 10:37:20 -0500

    fixed some errors and made the code work

--- app/src/main/java/dev/atomtables/atomchat/MainActivity.java
+++ app/src/main/java/dev/atomtables/atomchat/MainActivity.java
@@ -3,16 +3,19 @@ package dev.atomtables.atomchat;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ScrollView sv = findViewById(R.id.scr1);
+        sv.post(() -> sv.fullScroll(View.FOCUS_DOWN));
 
         Button button = findViewById(R.id.send_button);
         usernameSet("onLaunch");
         button.setOnClickListener(v -> onSendMessage());
+        onReceiveMessage("received :/");
     }
     final int[] i = {1};
 
@@ -148,8 +154,51 @@ public class MainActivity extends AppCompatActivity {
             case R.id.user_change:
                 usernameSet("onCall");
                 return true;
+            case R.id.help:
+                Intent i = new Intent(this, HelpActivity.class);
+                startActivity(i);
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    public void onReceiveMessage(String message) {
+
+        RelativeLayout relativeLayout = new RelativeLayout(MainActivity.this);
+           LinearLayout parentLayout = findViewById(R.id.parentLayout);
+           RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            TextView message_new = new TextView(MainActivity.this);
+            message_new.setText(message);
+            message_new.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.received_message_shape));
+            message_new.setTextColor(Color.parseColor("#000000"));
+            message_new.setTextSize(16);
+
+            RelativeLayout.LayoutParams tlp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            TextView username = new TextView(MainActivity.this);
+            username.setText(callUsername("", "no"));
+            username.setTextColor(Color.parseColor("#f3f3f3"));
+            username.setTextSize(12);
+            message_new.setId(i[0]);
+
+            RelativeLayout.LayoutParams ulp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            ulp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+            tlp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+            ulp.addRule(RelativeLayout.BELOW, i[0]);
+            i[0]++;
+            rlp.setMargins(10, 10, 5, 0);
+            relativeLayout.setLayoutParams(rlp);
+            message_new.setLayoutParams(tlp);
+            username.setLayoutParams(ulp);
+            relativeLayout.addView(message_new);
+            relativeLayout.addView(username);
+            parentLayout.addView(relativeLayout);
     }
 }
