5bb6f88c5f8da8de8d68087a50ecf00093b24861
atomtables <adithv08@gmail.com>
2023-02-04 19:12:25 -0500

    added client time stamp to each message

--- app/src/main/java/dev/atomtables/atomchat/MainActivity.java
+++ app/src/main/java/dev/atomtables/atomchat/MainActivity.java
@@ -23,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -251,7 +252,8 @@ public class MainActivity extends AppCompatActivity {
         socket.on("new_message", (arg) -> runOnUiThread(() -> {
             String arg2 = arg[0].toString();
             String[] arg3 = arg2.split("%&##\uE096%%@");
-            onReceiveMessage(arg3[0], arg3[1]);
+            System.out.println(Arrays.toString(arg3));
+            onReceiveMessage(arg3[0], arg3[1], arg3[2]);
             typingUsers.remove(arg3[1]);
             String currentTypingPeople;
             StringBuilder listPeople = new StringBuilder();
@@ -310,61 +312,70 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSendMessage() {
         System.out.println("current status : " + socket.connected());
-        EditText send_message = findViewById(R.id.send_message);
-        String message = send_message.getText().toString();
-        send_message.requestFocus();
-        send_message.setHint("Message");
-        send_message.setHintTextColor(Color.parseColor("#757575"));
-        if (!message.equals("")) {
-            send_message.setText("");
-            RelativeLayout relativeLayout = new RelativeLayout(MainActivity.this);
-            LinearLayout parentLayout = findViewById(R.id.parentLayout);
-            RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-            );
-            TextView message_new = new TextView(MainActivity.this);
-            message_new.setText(message);
-            message_new.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.sent_message_shape));
-            message_new.setTextColor(Color.parseColor("#000000"));
-            message_new.setTextSize(16);
-
-            RelativeLayout.LayoutParams tlp = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-            );
-            TextView username = new TextView(MainActivity.this);
-            username.setText(current_username);
-            username.setTextColor(Color.parseColor("#f3f3f3"));
-            username.setTextSize(12);
-            message_new.setId(i[0]);
-
-            RelativeLayout.LayoutParams ulp = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT
-            );
-            ulp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-            tlp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-            ulp.addRule(RelativeLayout.BELOW, i[0]);
-            i[0]++;
-            rlp.setMargins(10, 10, 5, 0);
-            relativeLayout.setLayoutParams(rlp);
-            message_new.setLayoutParams(tlp);
-            username.setLayoutParams(ulp);
-            relativeLayout.addView(message_new);
-            relativeLayout.addView(username);
-            parentLayout.addView(relativeLayout);
-            socket.emit("send_message", message, current_username);
-            ScrollView sv = findViewById(R.id.scr1);
-            sv.post(() -> sv.fullScroll(View.FOCUS_DOWN));
-        } else {
-            send_message.setHint("Please type a message");
-            send_message.setHintTextColor(Color.parseColor("#eb4034"));
+        if (socket.connected()) {
+            EditText send_message = findViewById(R.id.send_message);
+            String message = send_message.getText().toString();
+            send_message.requestFocus();
+            send_message.setHint("Message");
+            send_message.setHintTextColor(Color.parseColor("#757575"));
+            if (!message.equals("")) {
+                @SuppressLint("SimpleDateFormat")
+                String timeStamp = new SimpleDateFormat("h:mm a").format(new java.util.Date());
+                send_message.setText("");
+                RelativeLayout relativeLayout = new RelativeLayout(MainActivity.this);
+                LinearLayout parentLayout = findViewById(R.id.parentLayout);
+                // layout params
+                RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                TextView message_new = new TextView(MainActivity.this);
+                message_new.setText(message);
+                message_new.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.sent_message_shape));
+                message_new.setTextColor(Color.parseColor("#000000"));
+                message_new.setTextSize(16);
+                // new message params
+                RelativeLayout.LayoutParams tlp = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                TextView username = new TextView(MainActivity.this);
+                // data for underlying setText
+                String datsa = current_username + " • " + timeStamp;
+                username.setText(datsa);
+                username.setTextColor(Color.parseColor("#f3f3f3"));
+                username.setTextSize(12);
+                message_new.setId(i[0]);
+                // username params
+                RelativeLayout.LayoutParams ulp = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                ulp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+                tlp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+                ulp.addRule(RelativeLayout.BELOW, i[0]);
+                i[0]++;
+                rlp.setMargins(10, 10, 5, 0);
+                relativeLayout.setLayoutParams(rlp);
+                message_new.setLayoutParams(tlp);
+                username.setLayoutParams(ulp);
+                relativeLayout.addView(message_new);
+                relativeLayout.addView(username);
+                parentLayout.addView(relativeLayout);
+                socket.emit("send_message", message, current_username, timeStamp);
+                ScrollView sv = findViewById(R.id.scr1);
+                sv.post(() -> sv.fullScroll(View.FOCUS_DOWN));
+            } else {
+                send_message.setHint("Please type a message");
+                send_message.setHintTextColor(Color.parseColor("#eb4034"));
+            }
         }
-
+        else {
+            onConnectionIssue();
+        }
     }
 
-    public void onReceiveMessage(String message, String user) {
+    public void onReceiveMessage(String message, String user, String time) {
         RelativeLayout relativeLayout = new RelativeLayout(MainActivity.this);
         LinearLayout parentLayout = findViewById(R.id.parentLayout);
         RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
@@ -382,7 +393,9 @@ public class MainActivity extends AppCompatActivity {
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
         TextView username = new TextView(MainActivity.this);
-        username.setText(user);
+        // data for underlying setText
+        String datsa = user + " • " + time;
+        username.setText(datsa);
         username.setTextColor(Color.parseColor("#f3f3f3"));
         username.setTextSize(12);
         message_new.setId(i[0]);
@@ -421,7 +434,6 @@ public class MainActivity extends AppCompatActivity {
         send_message.setHint("Connection Issue");
         send_message.setHintTextColor(Color.parseColor("#eb4034"));
         send_message.setText("");
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
